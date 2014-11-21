360ea48217741f5cc79498f4ba9f9889f6047185
Luis G.F <luisgf@luisgf.es>
2014-11-21 18:33:55 +0100

    Arreglos en el script de ejecucion de tests

--- openbadges/runtests.sh
+++ openbadges/runtests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-rm private/* && rm public/* && /home/luisgf/venv-openbadges/bin/python3 -m unittest discover -s tests/ 
+rm private/* >> /dev/null
+rm public/* >>/dev/null
+/home/luisgf/venv-openbadges/bin/python3 -m unittest discover -s tests/
+
 
