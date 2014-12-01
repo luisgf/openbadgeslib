703828cf91d20fec0642092c30f7922430549358
Luis G.F <luisgf@luisgf.es>
2014-12-01 14:39:04 +0100

    Documentacion desplazada dentro del paquete. Paquete renombrado a openbadgeslib

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
  
 from distutils.core import setup
 setup(
-  name = 'openbadges',
-  packages = ['openbadges'], # this must be the same as the name above
+  name = 'openbadgeslib',
+  packages = ['openbadgeslib'], # this must be the same as the name above
   version = '0.1',
-  description = 'A openbadge implementation',
+  description = 'A library to sign a verify OpenBadges',
   author = 'Luis Gonzalez Fernandez',
   author_email = 'luisgf@luisgf.es',
   url = 'https://hg.luisgf.es/openbadges', # use the URL to the github repo
   download_url = 'https://hg.luisgf.es/openbadges/tarball/0.1', # I'll explain this in a second
-  keywords = ['openbadge'], # arbitrary keywords
+  keywords = ['openbadges'], # arbitrary keywords
   classifiers = [],
 )
