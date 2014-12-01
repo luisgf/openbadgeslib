f0f63f0d57c89d8540bc54aefd844e8989276492
Luis G.F <luisgf@luisgf.es>
2014-12-01 14:21:21 +0100

    Añadida licencia a fichero config.py

--- openbadges/config.py
+++ openbadges/config.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
+"""
+        LibOpenBadges
 
+        Copyright (c) 2014, Luis Gonzalez Fernandez, All rights reserved.
+
+        This library is free software; you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public
+        License as published by the Free Software Foundation; either
+        version 3.0 of the License, or (at your option) any later version.
+
+        This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+        Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public
+        License along with this library.
+"""
 """ 
     Please, don't enable this if you are not completly sure 
     that your are doing.
