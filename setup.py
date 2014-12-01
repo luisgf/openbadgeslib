55501c07afa0572d32031405f905e2744c7c5620
Luis G.F <luisgf@luisgf.es>
2014-12-01 15:01:42 +0100

    Preparacion de proyecto para envio a pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
   version = '0.1',
   description = 'A library to sign a verify OpenBadges',
   author = 'Luis Gonzalez Fernandez',
-  author_email = 'luisgf@luisgf.es',
-  url = 'https://hg.luisgf.es/openbadges', # use the URL to the github repo
-  download_url = 'https://hg.luisgf.es/openbadges/tarball/0.1', # I'll explain this in a second
+  author_email = 'openbadges@luisgf.es',
+  url = 'https://openbadges.luisgf.es/', # use the URL to the github repo
+  download_url = 'https://hg.luisgf.es/tarball/0.1/', # I'll explain this in a second
   keywords = ['openbadges'], # arbitrary keywords
   classifiers = [],
 )
