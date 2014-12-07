5be98338a15c28a4cb334b4ca316c76be7eb7544
Jesus Cea <jcea@jcea.es>
2014-12-07 03:32:33 +0100

    Simplify hashing

--- openbadgeslib/util.py
+++ openbadgeslib/util.py
@@ -17,34 +17,17 @@
         You should have received a copy of the GNU Lesser General Public
         License along with this library.
 """
-"""    
-        Shared Util functions
-    
-        Author:   Luis Gonzalez Fernandez <luisgf@luisgf.es>
-        Date:     20141201
-        Version:  0.1
-"""
 
 import hashlib
 from .errors import HashError
 
-""" Shared Utils """
-def sha1_string(string):
-    """ Calculate SHA1 digest of a string """
-    try:
-        hash = hashlib.new('sha1')
-        hash.update(string)
-        return hash.hexdigest().encode('utf-8')     # hexdigest() return an 'str' not bytes.
-    except:
-        raise HashError() 
+def _hash_string(hash_name, string) :
+    h = hashlib.new(hash_name)
+    return h.hexdigest().encode('latin-1')
+
+def sha1_string(string) :
+    return _hash_string('sha1', string)
 
 def sha256_string(string):
-    """ Calculate SHA256 digest of a string """
-    try:
-        hash = hashlib.new('sha256')
-        hash.update(string)
-        return hash.hexdigest().encode('utf-8')     # hexdigest() return an 'str' not bytes.
-    except:
-        raise HashError() 
-  
+    return _hash_string('sha256', string)
 
