96699ca8c2cb0dc9f04baeff45c4a17f6a8c1ea1
Luis G.F <luisgf@luisgf.es>
2014-12-08 22:09:26 +0100

    Añadido fichero de parseo de configuracion

---
+++ openbadgeslib/confparser.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+        OpenBadges Library
+
+        Copyright (c) 2014, Luis González Fernández, luisgf@luisgf.es
+        Copyright (c) 2014, Jesús Cea Avión, jcea@jcea.es
+
+        This library is free software; you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public
+        License as published by the Free Software Foundation; either
+        version 3.0 of the License, or (at your option) any later version.
+
+        This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+        Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public
+        License along with this library.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+from configparser import ConfigParser, ExtendedInterpolation, Error, NoOptionError
+
+class ConfParser():
+    def __init__(self, config_file='config.ini'):
+        self.config_file = config_file 
+
+    def read_conf(self):
+        self.parser = ConfigParser(interpolation=ExtendedInterpolation())        
+        return self.parser.read(self.config_file)        
+
+if __name__ == '__main__':
+    pass
