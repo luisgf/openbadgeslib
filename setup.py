4e3b2fb20f221969ad8bc6dfc0904eef0b93b2d7
Jesus Cea <jcea@jcea.es>
2014-12-09 17:20:17 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 from distutils.core import setup
 
+version = '0.1'
+
 with open('requirements.txt', 'r') as fh:
     dependencies = [l.strip() for l in fh]
 
 setup(
   name = 'openbadgeslib',
   packages = ['openbadgeslib'], # this must be the same as the name above
-  version = '0.1',
+  version = version,
   description = 'A library to sign and verify OpenBadges',
-  author = 'Luis Gonzalez Fernandez',
-  author_email = 'openbadges@luisgf.es',
-  url = 'https://openbadges.luisgf.es/', # use the URL to the github repo
-  download_url = 'https://hg.luisgf.es/tarball/0.1/', # I'll explain this in a second
+  author = 'Luís González Fernández, Jesús Cea Avión',
+  author_email = 'openbadges@luisgf.es, jcea@jcea.es',
+  url = 'https://hg.luisgf.es/openbadges/',
   keywords = ['openbadges'], # arbitrary keywords
   classifiers = [
       'Development Status :: 3 - Alpha',
@@ -25,7 +26,6 @@ setup(
       'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
   ],
   license='LGPLv3',
-  requires=['urllib','hashlib','ssl','xml','json','time','sys','os'],
   install_requires=dependencies,
   package_dir={'openbadgeslib': './openbadgeslib'},
   package_data={'openbadgeslib': ['./3dparty/jws/*.*','requirements.txt']},
@@ -37,6 +37,5 @@ setup(
 	      'openbadges-verifier = openbadgeslib.openbadges_verifier:main'
           ]
   }
-
 )
 
