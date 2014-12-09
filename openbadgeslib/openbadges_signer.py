11c16ab43a76862e36f89bf3b45fb101171d4d42
Luis G.F <luisgf@luisgf.es>
2014-12-09 12:32:18 +0100

    config.ini support added

--- openbadgeslib/openbadges_signer.py
+++ openbadgeslib/openbadges_signer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-    Copyright (c) 2014, Luís González Fernández - luisgf@luisgf.es
+    Copyright (c) 2014, Luis González Fernández - luisgf@luisgf.es
     Copyright (c) 2014, Jesús Cea Avión - jcea@jcea.es
     All rights reserved.
 
@@ -29,52 +29,74 @@
 """
 
 import argparse
+import sys, os
 
-from config import profiles  # XXX
 from .signer import SignerFactory
-from .errors import LibOpenBadgesException, SignerExceptions, BadgeNotFound
+from .errors import LibOpenBadgesException, SignerExceptions
+from .confparser import ConfParser
 
 # Entry Point
 def main():
     parser = argparse.ArgumentParser(description='Badge Signer Parameters')
+    parser.add_argument('-c', '--config', default='config.ini', help='Specify the config.ini file to use')
     parser.add_argument('-b', '--badge', required=True, help='Specify the badge name for sign')
     parser.add_argument('-r', '--receptor', required=True, help='Specify the receptor email of the badge')
     parser.add_argument('-o', '--output', required=True, help='Specify the output directory to save the badge.')
-    parser.add_argument('-p', '--profile', required=True, help='Specify the profile to use')
     parser.add_argument('-e', '--evidence', help='Set an url to the user evidence')
     parser.add_argument('-d', '--debug', action="store_true", help='Show debug messages in runtime.')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1' )
     args = parser.parse_args()
 
     if args.badge:
-         # Check if the profile exists
-        try:
-            config = profiles[args.profile]
-        except KeyError:
-            print('Profile %s not exist in config.py' % args.profile)
+        cf = ConfParser(args.config)
+        conf = cf.read_conf()
+
+        if not conf:
+            print('[!] The config file %s NOT exists or is empty' % args.config)
+            sys.exit(-1)
+
+        if not conf[args.badge]:
+            print('%s is not defined in this config file' % args.badge)
+            sys.exit(-1)
 
         try:
-            sf = SignerFactory(config, args.badge, args.receptor, evidence=args.evidence, debug_enabled=args.debug)
+            _badge_file_in = conf['paths']['base_image'] + '/' + conf[args.badge]['image']
+            _badge_svg_in = conf['issuer']['publish_url'] + '/' + conf[args.badge]['image']
+            _badge_json_url = conf['issuer']['publish_url'] + '/' + args.badge + '.json'
+            _badge_verify_key_url = conf[args.badge]['verify_key']
+            _priv_key = conf['keys']['private']
+            _pub_key = conf['keys']['public']
+
+            if not os.path.isfile(_badge_file_in):
+                print('[!] Badge file %s NOT exists.' % _badge_file_in)
+                sys.exit(-1)
+
+            """ Reading the SVG content """
+            with open(_badge_file_in,"rb") as f:
+                _badge_image_data = f.read()
+
+            """ Reading the keys """
+            with open(_priv_key,"rb") as f:
+                _priv_key_pem = f.read()
+
+            with open(_pub_key,"rb") as f:
+                _pub_key_pem = f.read()
+
+            sf = SignerFactory(badge_name=args.badge, badge_image=_badge_svg_in, badge_json_url=_badge_json_url, receptor=args.receptor, evidence=args.evidence, debug_enabled=args.debug)
             print("[!] Generating signature for badge '%s'..." % args.badge)
 
-            # Output file...
-            badgeout = sf.generate_output_filename(args.output, args.receptor)
+            _badge_file_out = sf.generate_output_filename(_badge_file_in, args.output, args.receptor)
+            _badge_assertion = sf.generate_openbadge_assertion(_priv_key_pem, _pub_key_pem)
+            _badge_svg_out = sf.svg_sign(_badge_svg_in, _badge_assertion)
 
-            if sf.sign_svg_file(badgeout):
-                print('[+] Badge Signed succesfully at: ', badgeout)
+            if _badge_svg_out:
+                print('[+] Badge Signed succesfully at: ', _badge_file_out)
             else:
                 print('[-] An error has occurred during signing the badge.')
-
-        except BadgeNotFound:
-            print('%s is not defined or not attached as badge to this profile.' % args.badge)
         except SignerExceptions:
             raise
         except LibOpenBadgesException:
             raise
-
-    else:
-        parser.print_help()
-
 
 if __name__ == '__main__':
     main()
--- openbadgeslib/signer.py
+++ openbadgeslib/signer.py
@@ -54,88 +54,69 @@ def SignerFactory(key_type='RSA'):
 class SignerBase():
     """ JWS Signer Factory """
 
-    def __init__(self, issuer, badgename, receptor, evidence, debug_enabled):
-        self.issuer = issuer.encode('utf-8')
-        self.badgename = badgename.encode('utf-8')
-        self.receptor = receptor.encode('utf-8')      # Receptor of the badge
-        self.evidence = evidence                      # Url to the user evidence
+    def __init__(self, issuer, badge_name, badge_file_path, badge_image_url, badge_json_url, receptor, evidence, verif_key_url, debug_enabled):
+        self._issuer = issuer.encode('utf-8')
+        self._badge_name = badge_name.encode('utf-8')
+        self._badge_file_path = badge_file_path         # Path to loca file
+        self._badge_image_url = badge_image_url
+        self._badge_json_url = badge_json_url
+        self._receptor = receptor.encode('utf-8')      # Receptor of the badge
+        self._evidence = evidence                      # Url to the user evidence
+        self._verif_key_url = verif_key_url
         self.in_debug = debug_enabled                 # Debug mode enabledl
 
     def generate_uid(self):
-        """ Generate a UID for a signed badge """
-
-        return sha1_string((self.conf['issuer']['name'] + self.badge['name']).encode() + self.receptor)
+        return sha1_string(self._issuer + self._badge_name + self._receptor + int(time.time()))
 
     def generate_jws_payload(self):
-        """ Generate JWS Payload """
 
         # All this data MUST be a Str string in order to be converted to json properly.
         recipient_data = dict (
-            identity = (b'sha256$' + sha256_string(self.receptor)).decode('utf-8'),
+            identity = (b'sha256$' + sha256_string(self._receptor)).decode('utf-8'),
             type = 'email',
             hashed = 'true'
         )
 
         verify_data = dict(
             type = 'signed',
-            url = self.badge['url_key_verif']
+            url = self._verif_key_url
         )
 
         payload = dict(
                         uid = self.generate_uid().decode('utf-8'),
                         recipient = recipient_data,
-                        image = self.badge['image'],
-                        badge = self.badge['json_url'],
+                        image = self._badge_image_url,
+                        badge = self._badge_json_url,
                         verify = verify_data,
                         issuedOn = int(time.time())
                      )
 
-        if self.evidence:
-            payload['evidence'] = self.evidence
+        if self._evidence:
+            payload['evidence'] = self._evidence
 
         self.debug('JWS Payload %s ' % json.dumps(payload))
 
         return payload
 
-    def sign_svg_file(self, file_out):
-        """ Add the Assertion information into the SVG file. """
+    def sign_svg(self, svg_in, assertion):
+        svg_doc = parseString(svg_in)
 
-        file_in = self.get_badge_local_path()
+        # Assertion
+        xml_tag = svg_doc.createElement("openbadges:assertion")
+        xml_tag.attributes['xmlns:openbadges'] = 'http://openbadges.org'
+        svg_doc.childNodes[1].appendChild(xml_tag)
+        xml_tag.attributes['verify']= assertion.decode('utf-8')
+        svg_doc.childNodes[1].appendChild(xml_tag)
 
-        if not os.path.exists(file_in):
-            raise FileToSignNotExists()
-
-        if os.path.exists(file_out):
-            raise BadgeSignedFileExists('Output file exists at:', file_out)
-
-        try:
-
-            assertion = self.generate_openbadge_assertion()
-
-            # Parse de SVG XML
-            svg_doc = parse(file_in)
-
-            # Assertion
-            xml_tag = svg_doc.createElement("openbadges:assertion")
-            xml_tag.attributes['xmlns:openbadges'] = 'http://openbadges.org'
-            svg_doc.childNodes[1].appendChild(xml_tag)
-            xml_tag.attributes['verify']= assertion.decode('utf-8')
-            svg_doc.childNodes[1].appendChild(xml_tag)
-
-            """ Log the signing process before write the badge to disk.
+        """ Log the signing process before returning it.
                 That's prevents the existence of a signed badge without traces """
 
-            self.log(self.conf, '"%s" SIGNED successfully for receptor "%s" in file "%s"' % (self.badge['name'], self.receptor.decode('utf-8'), file_out))
+        self.log(self.conf, '"%s" SIGNED successfully for receptor "%s"' % (self._badge_name, self._receptor.decode('utf-8')))
 
-            with open(file_out, "w") as f:
-                svg_doc.writexml(f)
+        svg_signed = svg_doc.toxml()
+        svg_doc.unlink()
 
-        except:
-            raise ErrorSigningFile('Error Signing file: ', file_in)
-        finally:
-            svg_doc.unlink()
-
-        return True
+        return svg_signed
 
     def debug(self, msg):
         """ Show debug messages if debug mode is enabled """
@@ -143,77 +124,63 @@ class SignerBase():
         if self.in_debug:
             print('DEBUG:', msg)
 
-    def generate_output_filename(self, output_dir, receptor):
+    def generate_output_filename(self, file_in, output_dir, receptor):
         """ Generate an output filename based on the source
             name and the receptor email """
 
-        file_in = self.get_badge_local_path()
         fbase = os.path.basename(file_in)
         fname, fext = os.path.splitext(fbase)
         fsuffix = receptor.replace('@','_').replace('.','_')
 
         return output_dir + fname + '_'+ fsuffix + fext
 
-    def generate_openbadge_assertion(self):
+    def generate_openbadge_assertion(self, priv_key_pem, pub_key_pem):
         """ Generate and Sign and OpenBadge assertion """
 
         header = self.generate_jose_header()
         payload = self.generate_jws_payload()
 
-        # Read the keys from files
-        kf = KeyFactory(self.conf)
-        try:
-            kf.read_private_key()
-            kf.read_public_key()
-        except:
-            raise PrivateKeyReadError()
+        self.key.read_private_key(priv_key_pem)
+        self.key.read_public_key(pub_key_pem)
 
-        signature = jws_sign(header, payload, kf.get_priv_key())
+        signature = jws_sign(header, payload, self.key.get_priv_key())
         assertion = jws_utils.encode(header) + b'.' + jws_utils.encode(payload) + b'.' + jws_utils.to_base64(signature)
 
         # Verify the assertion just after the generation.
         vf = VerifyFactory(self.conf)
         vf.load_pubkey_inline(kf.get_pub_key_pem())
 
-        if not vf.verify_jws_signature(assertion, kf.get_pub_key()):
+        if not vf.verify_jws_signature(assertion, self.key.get_pub_key()):
             return None
         else:
             self.debug('Assertion %s' % assertion)
             return assertion
 
-    def get_badge_local_path(self):
-        """ Return the path to the badge file """
-
-        return self.badge['local_badge_path']
-
     def log(self, profile, msg):
-        """ Log in a file the signature event """
-
         with open(profile['signedlog'], "ab") as log:
             entry = time.strftime("%d/%m/%Y %H:%M:%S").encode('utf-8') + b' ' + msg.encode('utf-8') + b'\n'
             log.write(entry)
 
 class SignerRSA(SignerBase):
-    def __init__(self, badgename=None, receptor='', evidence=None, debug_enabled=False):
-         super().__init__(badgename, receptor, evidence, debug_enabled)
+    def __init__(self, badge_name=None, badge_image_url=None, badge_json_url=None, receptor='', evidence=None, verif_key_url=None, debug_enabled=False):
+        super().__init__(badge_name, badge_image_url, badge_json_url, receptor, evidence, verif_key_url, debug_enabled)
+        self.key = KeyFactory('RSA')
 
     def generate_jose_header(self):
-        """ Generate JOSE Header """
-
         jose_header = { 'alg': 'RS256' }
-        self.debug('JOSE HEADER %s ' % json.dumps(jose_header))
 
+        self.debug('JOSE HEADER %s ' % json.dumps(jose_header))
         return jose_header
 
 class SignerECC(SignerBase):
-    def __init__(self, badgename=None, receptor='', evidence=None, debug_enabled=False):
-         super().__init__(badgename, receptor, evidence, debug_enabled)
+    def __init__(self, badge_name=None, badge_image_url=None, badge_json_url=None, receptor='', evidence=None, verif_key_url=None, debug_enabled=False):
+        super().__init__(badge_name, badge_image_url, badge_json_url, receptor, evidence, verif_key_url, debug_enabled)
+        self.key = KeyFactory('ECC')
 
     def generate_jose_header(self):
-        """ Generate JOSE Header """
-
         jose_header = { 'alg': 'ES256' }
-        self.debug('JOSE HEADER %s ' % json.dumps(jose_header))
 
+        self.debug('JOSE HEADER %s ' % json.dumps(jose_header))
         return jose_header
+
 
