99017f273f4710a64c44f470dc7d587e6dd9a058
Luis G.F <luisgf@luisgf.es>
2014-12-10 09:08:26 +0100

    Fix license typo

--- openbadgeslib/confparser.py
+++ openbadgeslib/confparser.py
@@ -5,6 +5,8 @@
         Copyright (c) 2014, Luis González Fernández, luisgf@luisgf.es
         Copyright (c) 2014, Jesús Cea Avión, jcea@jcea.es
 
+        All rights reserved.
+
         This library is free software; you can redistribute it and/or
         modify it under the terms of the GNU Lesser General Public
         License as published by the Free Software Foundation; either
