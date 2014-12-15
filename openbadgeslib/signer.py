bb6cae60c0a93b1ffe042493414736691698b5bc
Luis G.F <luisgf@luisgf.es>
2014-12-15 16:20:14 +0100

    Improved svg tag detection in signature process

--- openbadgeslib/signer.py
+++ openbadgeslib/signer.py
@@ -104,17 +104,12 @@ class SignerBase():
         if (self.has_assertion(svg_doc)):
             raise ErrorSigningFile('The input SVG file is already signed.')
 
-        try:
-            xmlid = 1 if svg_doc.childNodes[1].nodeName == 'svg' else 0            
-        except IndexError:
-            xmlid = 0
-
         # Assertion
-        xml_tag = svg_doc.createElement("openbadges:assertion")
-        xml_tag.attributes['xmlns:openbadges'] = 'http://openbadges.org'
-        svg_doc.childNodes[xmlid].appendChild(xml_tag)
-        xml_tag.attributes['verify']= assertion.decode('utf-8')
-        svg_doc.childNodes[xmlid].appendChild(xml_tag)
+        svg_tag = svg_doc.getElementsByTagName('svg').item(0)
+        assertion_tag = svg_doc.createElement("openbadges:assertion")
+        assertion_tag.attributes['xmlns:openbadges'] = 'http://openbadges.org'        
+        assertion_tag.attributes['verify']= assertion.decode('utf-8')
+        svg_tag.appendChild(assertion_tag)
 
         """ Log the signing process before returning it.
                 That's prevents the existence of a signed badge without traces """
