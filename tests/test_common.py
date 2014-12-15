328f4b0b9a742fe29d1cfbe6e9e396c6b4d4efa6
Jesus Cea <jcea@jcea.es>
2014-12-15 01:22:16 +0100

    Move tests outside the installed library

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,7 +1,7 @@
 import sys, os.path
 
 path = os.path.dirname(__file__)
-path = os.path.join(path, os.path.pardir, os.path.pardir)
+path = os.path.join(path, os.path.pardir)
 if sys.path[0] != path :
     sys.path.insert(0, path)
 
