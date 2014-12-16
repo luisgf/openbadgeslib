e8163f54632eb62758c2a5caf80dac2b3f08ea1e
Luis G.F <luisgf@luisgf.es>
2014-12-16 21:28:02 +0100

    Added default param deterministic=False for signer class constructor

--- openbadgeslib/signer.py
+++ openbadgeslib/signer.py
@@ -56,7 +56,7 @@ def SignerFactory(key_type='RSA', *args, **kwargs):
 class SignerBase():
     """ JWS Signer Factory """
 
-    def __init__(self, issuer='', badge_name='', badge_file_path=None, badge_image_url=None, badge_json_url=None, receptor='', evidence=None, verify_key_url=None):
+    def __init__(self, issuer='', badge_name='', badge_file_path=None, badge_image_url=None, badge_json_url=None, receptor='', evidence=None, verify_key_url=None, deterministic=False):
         self.issuer = issuer.encode('utf-8')
         self.badge_name = badge_name.encode('utf-8')
         self.badge_file_path = badge_file_path       # Path to local file
@@ -65,7 +65,7 @@ class SignerBase():
         self.receptor = receptor.encode('utf-8')     # Receptor of the badge
         self.evidence = evidence                     # Url to evidence
         self.verify_key_url = verify_key_url
-        self.deterministic = False                   # Randomness
+        self.deterministic = deterministic           # Randomness
 
     def generate_uid(self):
         return sha1_string(self.issuer + self.badge_name + self.receptor + datetime.now().isoformat().encode('utf-8'))
