luisgf/openbadgeslib

commit 68b1531cbbc4e195c0cbb26aec457bbdf63afe1a

Version update to v0.4.2

openbadgeslib/badge.py
@@ -2,8 +2,8 @@
 """
         OpenBadges Library
 
-        Copyright (c) 2015, Luis González Fernández, luisgf@luisgf.es
-        Copyright (c) 2015, Jesús Cea Avión, jcea@jcea.es
+        Copyright (c) 2014-2015, Luis González Fernández, luisgf@luisgf.es
+        Copyright (c) 2014-2015, Jesús Cea Avión, jcea@jcea.es
 
         All rights reserved.
 
@@ -168,6 +168,49 @@ class Badge():
     def __str__(self):
         return 'INI Name: %s\nName: %s\nDescription: %s\nImage Type: %s\nImage Url: %s\nKey Type: %s\nVerify Key: %s\nJSON Url: %s\n' % (self.ini_name, self.name, self.description, self.image_type, self.image_url, self.key_type, self.verify_key_url, self.json_url)
 
+    def urls_has_problems(self):
+        """ Check if urls in Badge are corrects and online """
+
+        error = False
+        data = None
+
+        try:
+            data = download_file(self.image_url)
+        except:
+            pass
+        finally:
+            if not data:
+                print('[!] OpenBadge Image at config file is pointing to a wrong url: %s' % self.image_url)
+                error = True
+
+        try:
+            data = download_filie(self.criteria_url)
+        except:
+            pass
+        finally:
+            if not data:
+                print('[!] OpenBadge Criteria at config file is pointing to a wrong url %s' % self.criteria_url)
+                error = True
+
+        try:
+            data = download_file(self.json_url)
+        except:
+            pass
+        finally:
+            if not data:
+                print('[!] OpenBadge JSon at config file is pointing to a wrong url %s' % self.json_url)
+                error = True
+
+        try:
+            data = download_file(self.verify_key_url)
+        except:
+            pass
+        finally:
+            if not data:
+                print('[!] OpenBadge Verify key at config file is poiting to a wrong url %s' % self.verify_key_url)
+                error = True
+
+        return error
 
 class BadgeSigned():
     """ A Signed Badge Object """
@@ -214,9 +257,15 @@ class BadgeSigned():
         except KeyError:
             expiration=None
 
-        # TODO: Download from internet the files associated
-        pubkey_pem = download_file(body['verify']['url'])
-        key_type = detect_key_type(pubkey_pem)
+        try:
+            pubkey_pem = download_file(body['verify']['url'])
+            key_type = detect_key_type(pubkey_pem)
+        except:
+            pubkey_pem = None
+            key_type = None
+            print('Unable to verify OpenBadge Signature. The URL pointing to verify key doesn\'t exists.')
+            print('Url with problems: %s' % body['verify']['url'])
+            sys.exit(-1)
 
         badge = Badge(image_url=body['image'], verify_key_url=body['verify']['url'],
                       json_url=body['badge'], key_type=key_type,
@@ -254,6 +303,11 @@ class BadgeSigned():
 
     def __str__(self):
         return 'Serial Num: %s\nIdentity: %s\nEvidence %s\nExpiration: %s\nSalt: %s\n' % (self.serial_num, self.identity, self.evidence, self.expiration, self.salt)
+
+    def get_signkey_pem(self):
+        """ Return the public key pem used to sign the openbadge """
+
+        return self.source.pubkey_pem
 
 def extract_svg_assertion(file_data):
     """ Extract the assertion embeded in a SVG file. """

MANIFEST
@@ -1,0 +1,54 @@
+# file GENERATED by distutils, do NOT edit
+CONTRIBUTORS.txt
+Changelog.txt
+LICENSE.txt
+MANIFEST.in
+README.txt
+TODO.txt
+setup.cfg
+setup.py
+docs/authors.rst
+docs/conf.py
+docs/development.rst
+docs/glossary.rst
+docs/index.rst
+docs/installation.rst
+docs/intro.rst
+docs/userguide.rst
+openbadgeslib/__init__.py
+openbadgeslib/badge.py
+openbadgeslib/config.ini.example
+openbadgeslib/confparser.py
+openbadgeslib/errors.py
+openbadgeslib/keys.py
+openbadgeslib/logs.py
+openbadgeslib/mail.py
+openbadgeslib/openbadges_init.py
+openbadgeslib/openbadges_keygenerator.py
+openbadgeslib/openbadges_publish.py
+openbadgeslib/openbadges_signer.py
+openbadgeslib/openbadges_verifier.py
+openbadgeslib/signer.py
+openbadgeslib/util.py
+openbadgeslib/verifier.py
+openbadgeslib/3dparty/jws/__init__.py
+openbadgeslib/3dparty/jws/algos.py
+openbadgeslib/3dparty/jws/exceptions.py
+openbadgeslib/3dparty/jws/header.py
+openbadgeslib/3dparty/jws/utils.py
+tests/config1.ini
+tests/logo Python España.svg
+tests/runtests.sh
+tests/test_common.py
+tests/test_key_operation.py
+tests/test_sign_ecc.pem
+tests/test_sign_rsa.pem
+tests/test_signer_operation.py
+tests/test_verify_ecc.pem
+tests/test_verify_operation.py
+tests/test_verify_rsa.pem
+tests/withoutxmlheader.svg
+tests/withxmlheader.svg
+tests/images/sample1.png
+tests/images/sample1.svg
+tests/images/userimage01.svg

dist/openbadgeslib-0.4.2.tar.gz.asc
@@ -1,0 +1,17 @@
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1
+
+iQIcBAABAgAGBQJWdyZTAAoJEGvLwn/JGLgPezUQALYGbIHm6rpGcLpTKeye3Pqb
+xu4MRfyvkxt22aDhbScRPf6KH7a14wlPgAZNg/3UdpvHvxxosXFVX/bjx40deOH8
+n/LONK1m0Opl4EoG1jE86Lkoz4DGGDZVsJINr9aM+ehbgqwuaxBAhUcn1oCQDFqv
+omGCvh/WxlM5ZkzYXNcNS6asp6y1VcHt4B0oJNrEQoCCVN06WlfKzHFr3OFYG+tl
+htUKKJ7f7IPFMZsFrw+k/BsxdsORsBhKaEGi9l1iCOQmFrtOjZV2Wq2W3GI5g4MB
+OYqTnrxgisqORva/w/1RD63FaYX1mDzd2KLW+v4+jBa7KaM4KMUa4tk21CT7Y98Q
+YGwkEhKtDgOQTA5w0rBZnJlLKbh8WPswXq0hPmA7OMQ+1MZhSGxH/AuRlSs9BCue
+nVSyHXCr15UnpJ24xGtzLBXhWmwCg5wm1SQRwrj6Fvkf0UXamwlJV/JiK10Pz1ih
+J4V/Qu1e9fmzZvy0c2VIHYjfoPmFX32vkyUEbjAX9DLQl4kSOw2qZRtVPVYGlkPJ
+PUR9SYuLGdgb9D7+ySjWYQF1yOkoosz9nQTDCBUHrmWugMgdVNano6uSIbUgYdS7
+XNaFoTZWOcH5lhsDO/qjhkqSRo90kRc4be68+pqbeohco35qFZ9IuI1kOPybJk7b
+NYlS8Qjd3+JTJsXpNRKd
+=MU59
+-----END PGP SIGNATURE-----

dist/openbadgeslib-0.4.2.zip.asc
@@ -1,0 +1,17 @@
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1
+
+iQIcBAABAgAGBQJWdyZ3AAoJEGvLwn/JGLgPAHIQAK9GJOVjFI9Tr6IWobfu+XKz
+DY6macD0x/2zW9LcNLqL0Nh/UGUymiu3BWNEF7Op1bBvkw7lkbptfUe0e3n1pZjf
+lHgfKaJuu5GgyLq2hM4YqAoTz+OKczmEjz7BlICmK01zQ+MIpLoIRfDdyxDGpjlD
+mL+2eHw/DMvoks87YFb8BaPml5gvicwrV2C2zCF27sfIYO0OlTyHkYGSL6aOfKEx
+DqeQX0n3dszK2VRBOR4CyIuyN7Ptn46gV1ogym+CTmJ2XXfOhfoEhiZlKDrsenP5
+0nKjbT0IGoAA1/991MSkYcgylG2JkeeoGS36FXSVFdbH4AWlj5RYlMCi20tx8k3s
+k0DCWWFGnKBsYEn+4TnxR4iiVj8mrijD3Y8Q2J/yJDfy5If2K4RUtlmfzVAtGgXB
+erCQmC/51fZ4KR+j5RqoY98ab74o4wEDz1Ibe5BNhw4EKzrbjLabchh4DGrj0TyT
+GUg1r0Fq9RJtuRRh+BXKyCE7X6HGozRZaB4Ofqyj3OApJ0uEfX4rU8gocZEs7LuG
+nBeqa+kuFsEAjDET0ay/wbaxon1Rksj67FlMWYKho8MOJRAND3NcelWAjj1Us4de
+H6/YMi0UbVorIllbwY4lE0rdS3klmBLbp+CZpC0HQ0ilMWyf3hQx8Nn7ZLjGYFo+
+iyOmApd2UdK1AT8HXS6a
+=jSmf
+-----END PGP SIGNATURE-----

docs/conf.py
@@ -1,0 +1,357 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# OpenBadgesLib documentation build configuration file, created by
+# sphinx-quickstart on Wed Mar 11 08:43:53 2015.
+#
+# This file is execfile()d with the current directory set to its
+# containing dir.
+#
+# Note that not all possible configuration values are present in this
+# autogenerated file.
+#
+# All configuration values have a default; values that are commented out
+# serve to show the default.
+
+import sys
+import os
+import shlex
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('.'))
+#sys.path.append('/home/luisgf/sources/openbadges/openbadgeslib/')
+
+# -- General configuration ------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+#needs_sphinx = '1.0'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+ #   'sphinx.ext.autodoc',
+ #   'sphinx.ext.doctest',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+#templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+source_suffix = ['.rst']
+
+# The encoding of source files.
+#source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = 'OpenBadgesLib'
+copyright = '2014-2015, Luis González Fernández'
+author = 'Luis González Fernández'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '1.0'
+# The full version, including alpha/beta/rc tags.
+release = '0.4.2'
+
+# The language for content autogenerated by Sphinx. Refer to documentation
+# for a list of supported languages.
+#
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set "language" from the command line for these cases.
+language = None
+
+# There are two options for replacing |today|: either, you set today to some
+# non-false value, then it is used:
+#today = ''
+# Else, today_fmt is used as the format for a strftime call.
+#today_fmt = '%B %d, %Y'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = []
+
+# The reST default role (used for this markup: `text`) to use for all
+# documents.
+#default_role = None
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+#add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+#add_module_names = True
+
+# If true, sectionauthor and moduleauthor directives will be shown in the
+# output. They are ignored by default.
+#show_authors = False
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# A list of ignored prefixes for module index sorting.
+#modindex_common_prefix = []
+
+# If true, keep warnings as "system message" paragraphs in the built documents.
+#keep_warnings = False
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ['../_themes']
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+#html_title = None
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+#html_short_title = None
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#html_logo = None
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+
+# Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+html_extra_path = ['../copy-to-web/']
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+#html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+#html_use_smartypants = True
+
+# Custom sidebar templates, maps document names to template names.
+#html_sidebars = {}
+
+# Additional templates that should be rendered to pages, maps page names to
+# template names.
+#html_additional_pages = {}
+
+# If false, no module index is generated.
+html_domain_indices = False
+
+# If false, no index is generated.
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = True
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = True
+
+# If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. ".xhtml").
+#html_file_suffix = None
+
+# Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'h', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'r', 'sv', 'tr'
+#html_search_language = 'en'
+
+# A dictionary with options for the search language support, empty by default.
+# Now only 'ja' uses this config value
+#html_search_options = {'type': 'default'}
+
+# The name of a javascript file (relative to the configuration directory) that
+# implements a search results scorer. If empty, the default will be used.
+#html_search_scorer = 'scorer.js'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'OpenBadgesLibdoc'
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+'papersize': 'a4paper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
+
+# Latex figure (float) alignment
+#'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+  (master_doc, 'OpenBadgesLib.tex', 'OpenBadgesLib Documentation',
+   'Luis González Fernández', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'openbadgeslib', 'OpenBadgesLib Documentation',
+     [author], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  (master_doc, 'OpenBadgesLib', 'OpenBadgesLib Documentation',
+   author, 'OpenBadgesLib', 'A library to sign a verify OpenBadges.',
+   'Miscellaneous'),
+]
+
+# Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+
+# If false, no module index is generated.
+#texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
+
+# If true, do not generate a @detailmenu in the "Top" node's menu.
+#texinfo_no_detailmenu = False
+
+
+# -- Options for Epub output ----------------------------------------------
+
+# Bibliographic Dublin Core info.
+#epub_title = project
+#epub_author = author
+#epub_publisher = author
+#epub_copyright = copyright
+
+# The basename for the epub file. It defaults to the project name.
+#epub_basename = project
+
+# The HTML theme for the epub output. Since the default themes are not optimized
+# for small screen space, using the same theme for HTML and epub output is
+# usually not wise. This defaults to 'epub', a theme designed to save visual
+# space.
+#epub_theme = 'epub'
+
+# The language of the text. It defaults to the language option
+# or 'en' if the language is not set.
+#epub_language = ''
+
+# The scheme of the identifier. Typical schemes are ISBN or URL.
+#epub_scheme = ''
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#epub_identifier = ''
+
+# A unique identification for the text.
+#epub_uid = ''
+
+# A tuple containing the cover image and cover page html template filenames.
+#epub_cover = ()
+
+# A sequence of (type, uri, title) tuples for the guide element of content.opf.
+#epub_guide = ()
+
+# HTML files that should be inserted before the pages created by sphinx.
+# The format is a list of tuples containing the path and title.
+#epub_pre_files = []
+
+# HTML files shat should be inserted after the pages created by sphinx.
+# The format is a list of tuples containing the path and title.
+#epub_post_files = []
+
+# A list of files that should not be packed into the epub file.
+#epub_exclude_files = ['search.html']
+
+# The depth of the table of contents in toc.ncx.
+#epub_tocdepth = 3
+
+# Allow duplicate toc entries.
+#epub_tocdup = True
+
+# Choose between 'default' and 'includehidden'.
+#epub_tocscope = 'default'
+
+# Fix unsupported image types using the Pillow.
+#epub_fix_images = False
+
+# Scale large images.
+#epub_max_image_width = 0
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#epub_show_urls = 'inline'
+
+# If false, no index is generated.
+#epub_use_index = True

openbadgeslib/__init__.py
@@ -2,8 +2,8 @@
 """
         OpenBadges Library
 
-        Copyright (c) 2015, Luis González Fernández, luisgf@luisgf.es
-        Copyright (c) 2015, Jesús Cea Avión, jcea@jcea.es
+        Copyright (c) 2014-2015, Luis González Fernández, luisgf@luisgf.es
+        Copyright (c) 2014-2015, Jesús Cea Avión, jcea@jcea.es
 
         All rights reserved.
 

commit 1c76e7e235b2704bff436113fc83ec6b66986d1f

badge: Fix 'download_filie' -> 'download_file' typo
Typo was introduced in 68b1531c (Version update to v0.4.2,
2015-12-20).

openbadgeslib/badge.py
@@ -184,7 +184,7 @@ class Badge():
                 error = True
 
         try:
-            data = download_filie(self.criteria_url)
+            data = download_file(self.criteria_url)
         except:
             pass
         finally: